fded1460aa5a201a22fb66b6451cce92329cf804
ivar henckel <henckelivar@gmail.com>
2023-03-15 12:24:09 +0100

    Initializing repo

---
+++ src/main/java/com/ivarsexample/calendardemo/CalendardemoApplication.java
@@ -0,0 +1,13 @@
+package com.ivarsexample.calendardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendardemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendardemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivarsexample/calendardemo/CalendardemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivarsexample.calendardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendardemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
